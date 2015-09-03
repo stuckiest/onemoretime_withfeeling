1c3081f35f6dced5131ef4a9c4c36824af9614e4
stuckiest <stuckiest@gmail.com>
2015-09-02 22:36:42 -0600

    hating this

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,20 @@ class PostsController < ApplicationController
       render :new
      end  
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end 
+  
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end       
 end  
 
 private
