d976b7bee4aad9726ee5641d540e9f6a87205674
stuckiest <stuckiest@gmail.com>
2015-09-02 22:21:57 -0600

    added navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-end
 
-def create
-  @post = Post.new(post_params)
-   if @post.save
-    redirect_to posts_path
-   else
-    render :new
-   end  
-end
+  def create
+    @post = Post.new(post_params)
+     
+     if @post.save
+      redirect_to posts_path
+     else
+      render :new
+     end  
+  end
+end  
 
 private
 def post_params
